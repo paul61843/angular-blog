1303e084369b9c7900e2f564a671c855e5483368
helio <paul61843@gmail.com>
2021-05-08 16:09:35 +0800

    [welcome 頁面] 大張畫面完成並顯示文字

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
+  welcome = {
+    title: 'Hello & Welcome',
+    sub: 'Whatever is worth doing is worth doing well.',
+  };
+
+  content = {
+    title: 'We are glad you see my blog!',
+    sub: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+    veniam, quis nostrud exercitatio.`,
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
